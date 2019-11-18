01889310235503e0d1212e77bce18aab7315773f
Marco Antonio Moreno <marcomorenoag@outlook.com>
2019-11-18 14:15:26 -0600

    Proyecto final listo :'D

--- Proyecto Final/master.py
+++ Proyecto Final/master.py
@@ -7,22 +7,34 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT, initial=GPIO.HIGH)
 
-send = serial.Serial(
-#    port='/dev/serial0',
+# Activa la comunicacion serial
+port = serial.Serial(
     port='/dev/ttyS0',
     baudrate = 115200,
-#    parity=serial.PARITY_NONE,
-#    stopbits=serial.STOPBITS_ONE,
-#    bytesize=serial.EIGHTBITS,
-#    timeout=1
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
 )
 
-# i = [0,10,45,90,135,180,135,90,45,10,0]
+
 cadena = ""
+contenido = []
 
 while True:
-	caracter = send.read()
+	caracter = port.read()
 	cadena += caracter
 	if(caracter == chr(13)):
-		print(cadena)
+		#Separamos el contenido de la cadena
+		contenido = cadena.split(',', 3)
+		print("Voltage: " + contenido[0])
+		print("Celsius: " + contenido[1])
+		print("Kelvin: " + contenido[2] + "\n")
+
+		voltage = contenido[0]
+		celsius = contenido[1]
+		kelvin = contenido[2]
+
+		contenido = []
+
 		cadena = ""
